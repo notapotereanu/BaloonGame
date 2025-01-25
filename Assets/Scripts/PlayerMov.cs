daf67fcd0f5e5b2981d317dfce3efb4e52617727
SebastianTuring <jsebas0830@gmail.com>
2025-01-25 11:21:21 -0500

    “InitialCommit”

---
+++ Assets/Scripts/PlayerMov.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMov : MonoBehaviour
+{
+    [SerializeField]
+    float movementSpeed;
+
+    void Update()
+    {
+        // Handle movement
+        MovePlayer();
+
+        // Handle flipping based on mouse position
+        FlipSprite();
+    }
+
+    void MovePlayer()
+    {
+        // Get input for horizontal and vertical movement
+        float moveX = Input.GetAxis("Horizontal"); // A/D or Left/Right Arrow
+        float moveY = Input.GetAxis("Vertical");   // W/S or Up/Down Arrow
+
+        // Move the player in 2D space
+        Vector2 movement = new Vector2(moveX, moveY);
+        transform.Translate(movement * movementSpeed * Time.deltaTime, Space.World);
+    }
+
+    void FlipSprite()
+    {
+        // Get the mouse position in world space
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        // Check if the mouse is to the left or right of the player
+        if (mousePosition.x < transform.position.x)
+        {
+            // Flip the sprite to face left
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else
+        {
+            // Flip the sprite to face right
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+    }
+}
+
+
---
+++ README.md
@@ -0,0 +1,1 @@
+This is a game where you play as a kid trying to protect a Balloon
