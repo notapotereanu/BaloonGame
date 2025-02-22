26505875c9a4a243e92b2b3be7e735b5e522dc08
Andrian Potereanu <apotereanu@gmail.com>
2025-02-22 14:37:35 +0100

    Functioning Main Menu, small bugfixes still to happen

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadSceneAsync("MainScene");
+        Debug.Log("MainScene loading");
+        // Game starts paused—ensure time is stopped and the pause menu is shown.
+        //Time.timeScale = 0f;
+        //isPaused = true; // Set isPaused to true to indicate the game is starting paused.
+        //if (pauseMenuPanel != null)
+        //    pauseMenuPanel.SetActive(true);
+    }
+
+
+    // Called by the Exit Game button
+    public void ExitGame()
+    {
+        Debug.Log("Exiting Game");
+        Application.Quit();
+    }
+}
