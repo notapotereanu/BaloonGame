1e4ecdfc03864cd3ba167990f31823ec0a53533f
4665696c6572 <466569.6c6572.55929@slmail.me>
2025-02-26 18:06:44 +0100

    Fixed some location issues
    Fixed cars so they collide.
    Made the benches smaller.
    Adjusted Z axis layers.
    Set box collider on Child

--- Assets/Scripts/PlayerMov.cs
+++ Assets/Scripts/PlayerMov.cs
@@ -7,6 +7,14 @@ public class PlayerMov : MonoBehaviour
     [SerializeField]
     float movementSpeed;
 
+    [SerializeField]
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
     void Update()
     {
         // Handle movement
@@ -23,8 +31,9 @@ public class PlayerMov : MonoBehaviour
         float moveY = Input.GetAxis("Vertical");   // W/S or Up/Down Arrow
 
         // Move the player in 2D space
-        Vector2 movement = new Vector2(moveX, moveY);
-        transform.Translate(movement * movementSpeed * Time.deltaTime, Space.World);
+        //Vector2 movement = new Vector2(moveX, moveY);
+        //transform.Translate(movement * movementSpeed * Time.deltaTime, Space.World);
+        rb.velocity = new Vector2(moveX, moveY) * movementSpeed;
     }
 
     void FlipSprite()
