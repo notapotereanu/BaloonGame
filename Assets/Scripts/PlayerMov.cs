9e39f68b7493bae35df746cdd68b4dfab9e975b9
4665696c6572 <466569.6c6572.55929@slmail.me>
2025-02-26 22:17:47 +0100

    Balloon Progress
    Balloon and child currently with physics.
    Camera Follow is disabled.

--- Assets/Scripts/PlayerMov.cs
+++ Assets/Scripts/PlayerMov.cs
@@ -1,4 +1,52 @@
-﻿using System.Collections;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class PlayerMov : MonoBehaviour
+//{
+//    [SerializeField]
+//    float movementSpeed;
+
+//    void Update()
+//    {
+//        // Handle movement
+//        MovePlayer();
+
+//        // Handle flipping based on mouse position
+//        FlipSprite();
+//    }
+
+//    void MovePlayer()
+//    {
+//        // Get input for horizontal and vertical movement
+//        float moveX = Input.GetAxis("Horizontal"); // A/D or Left/Right Arrow
+//        float moveY = Input.GetAxis("Vertical");   // W/S or Up/Down Arrow
+
+//        // Move the player in 2D space
+//        Vector2 movement = new Vector2(moveX, moveY);
+//        transform.Translate(movement * movementSpeed * Time.deltaTime, Space.World);
+//    }
+
+//    void FlipSprite()
+//    {
+//        // Get the mouse position in world space
+//        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+//        // Check if the mouse is to the left or right of the player
+//        if (mousePosition.x < transform.position.x)
+//        {
+//            // Flip the sprite to face left
+//            transform.localScale = new Vector3(-0.33f, 0.33f, 1);
+//        }
+//        else
+//        {
+//            // Flip the sprite to face right
+//            transform.localScale = new Vector3(0.33f, 0.33f, 1);
+//        }
+//    }
+//}
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -15,7 +63,7 @@ public class PlayerMov : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
     }
 
-    void Update()
+    void FixedUpdate()
     {
         // Handle movement
         MovePlayer();
@@ -54,5 +102,3 @@ public class PlayerMov : MonoBehaviour
         }
     }
 }
-
-
