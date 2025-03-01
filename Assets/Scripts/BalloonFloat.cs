7a45b74e3873899ead3929be0ebbdda7ab38565b
4665696c6572 <466569.6c6572.55929@slmail.me>
2025-03-01 21:01:30 +0100

    Revert "Merge remote-tracking branch 'origin/Level-3' into MenuBehaviour"
    This reverts commit 759204a7f768a3b40a9c7c0e0214e841eb441fae, reversing
    changes made to a80c2d9970259b4ce95f1f53cf0d3cdd8d80b5a2.

--- Assets/Scripts/BalloonFloat.cs
+++ Assets/Scripts/BalloonFloat.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using UnityEngine;
 
-
 public class BalloonFloat : MonoBehaviour
 {
     [SerializeField]
@@ -13,28 +12,77 @@ public class BalloonFloat : MonoBehaviour
     [SerializeField]
     private Transform hand;
 
+    float offset;
+
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-
+        offset = rb.position.y - hand.position.y;
     }
 
     void FixedUpdate()
     {
-        Bob();
+        Move();
+        Sway();
     }
 
-   
-    void Bob()
+    void Move()
     {
-        float moveY = Input.GetAxis("Vertical");
-        float moveX = Input.GetAxis("Horizontal");
+        // Base Movement
+        rb.AddForce(new Vector2((hand.position.x - rb.position.x), (hand.position.y - rb.position.y)) * force);
 
-        if (moveX == 0 && moveY == 0)
+        float maxY = hand.position.y + (offset * 1.1f);
+        float minY = hand.position.y + offset;
+
+        float maxX = hand.position.x + offset;
+        float minX = hand.position.x + offset * -1.0f;
+
+
+        //  Restricts balloon from falling too far behind on Y movement
+        if (transform.position.y > maxY) // Down
         {
-            rb.AddForce(Vector2.down * Mathf.Sin(Time.time) * force);
+            transform.position = new Vector2(transform.position.x, maxY);
+            rb.velocity = new Vector2(rb.velocity.x, 0f);
         }
-    
+        else if (transform.position.y < minY) // Up
+        {
+            transform.position = new Vector2(transform.position.x, minY);
+            rb.velocity = new Vector2(rb.velocity.x, 0f);
+        }
+
+
+        /*
+        * Note
+        * I kind of think Force looks better on X & Transform on Y. 
+        * I left a transform option in for everyone else to try
+        */
+        //  Restricts balloon from falling too far behind on X movement
+        if (transform.position.x > maxX) // Left
+        {
+            // Option 1
+            rb.AddForce(new Vector2(-1, 0) * force * 5f);
+
+            // option 2
+            //transform.position = new Vector2(maxX, transform.position.y);
+            //rb.velocity = new Vector2(0f, rb.velocity.x);
+        }
+        else if (transform.position.x < minX) // Right
+        {
+            rb.AddForce(new Vector2(1, 0) * force * 5f);
+        }
+    }
+
+
+    // Adds a swaying motion when child is not moving
+    void Sway()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
+
+        if (moveX == 0 && moveY == 0 && Math.Floor(Time.time) % 13 == 0)
+        {
+            rb.AddForce(new Vector2(1, 0) * force * 0.5f);
+        }
     }
 }
--- Assets/Scripts/PlayerMov.cs
+++ Assets/Scripts/PlayerMov.cs
@@ -1,87 +1,34 @@
-﻿//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
-
-//public class PlayerMov : MonoBehaviour
-//{
-//    [SerializeField]
-//    float movementSpeed;
-
-//    void Update()
-//    {
-//        // Handle movement
-//        MovePlayer();
-
-//        // Handle flipping based on mouse position
-//        FlipSprite();
-//    }
-
-//    void MovePlayer()
-//    {
-//        // Get input for horizontal and vertical movement
-//        float moveX = Input.GetAxis("Horizontal"); // A/D or Left/Right Arrow
-//        float moveY = Input.GetAxis("Vertical");   // W/S or Up/Down Arrow
-
-//        // Move the player in 2D space
-//        Vector2 movement = new Vector2(moveX, moveY);
-//        transform.Translate(movement * movementSpeed * Time.deltaTime, Space.World);
-//    }
-
-//    void FlipSprite()
-//    {
-//        // Get the mouse position in world space
-//        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-//        // Check if the mouse is to the left or right of the player
-//        if (mousePosition.x < transform.position.x)
-//        {
-//            // Flip the sprite to face left
-//            transform.localScale = new Vector3(-0.33f, 0.33f, 1);
-//        }
-//        else
-//        {
-//            // Flip the sprite to face right
-//            transform.localScale = new Vector3(0.33f, 0.33f, 1);
-//        }
-//    }
-//}
-
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerMov : MonoBehaviour
 {
     [SerializeField]
-    float movementSpeed;
+    private float movementSpeed; // Movement speed of the player
 
-    [SerializeField]
     private Rigidbody2D rb;
 
-    private void Start()
+    void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>(); // Get the Rigidbody2D component
     }
 
-    void FixedUpdate()
+    void Update()
     {
-        // Handle movement
-        MovePlayer();
-
         // Handle flipping based on mouse position
         FlipSprite();
     }
 
-    void MovePlayer()
+    void FixedUpdate()
     {
         // Get input for horizontal and vertical movement
         float moveX = Input.GetAxis("Horizontal"); // A/D or Left/Right Arrow
         float moveY = Input.GetAxis("Vertical");   // W/S or Up/Down Arrow
 
-        // Move the player in 2D space
-        //Vector2 movement = new Vector2(moveX, moveY);
-        //transform.Translate(movement * movementSpeed * Time.deltaTime, Space.World);
-        rb.velocity = new Vector2(moveX, moveY) * movementSpeed;
+        // Calculate the movement vector
+        Vector2 movement = new Vector2(moveX, moveY).normalized;
+
+        // Apply movement using velocity
+        rb.velocity = movement * movementSpeed;
     }
 
     void FlipSprite()
@@ -102,3 +49,5 @@ public class PlayerMov : MonoBehaviour
         }
     }
 }
+
+
